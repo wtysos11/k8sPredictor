0d9f9c6d00479392cd1d0dde5c74b91438f59390
wtysos11 <wtysos11@163.com>
2020-01-03 09:13:24 +0800

    ready to begin, test for upload.

---
+++ dataAccess.py
@@ -0,0 +1,2 @@
+# By using this script, it can download all the data from https://dumps.wikimedia.org/other/pageviews/ in the way I want.
+# I plan to download all the data first, then unzip and aggregate them.
