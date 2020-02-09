3a8604a18805cc28ec02e4c242f4f58306e1c74f
wtysos11 <wtysos11@163.com>
2020-02-09 16:20:17 +0800

    采用sklearn重写时间序列聚类算法

--- dataReader.py
+++ dataReader.py
@@ -102,6 +102,7 @@ for key in list(data.keys()):
 # 第二部分-1 文件存储
 # 进行数据备份与数据读取
 # 文件规定：以keykey开头下一行为键名，然后为一串int列表
+import os
 fileName = 'result.txt'
 dataPath = "E:\\code\\myPaper\\k8sPredictor"
 #返回一个词典，词典的key为第二项，value为一个列表，所有列表都应该等长
@@ -118,12 +119,10 @@ def ReadDataFromFile():
             readKey = False
             lastkey = line[:-1]#删去行尾换行符
             data[lastkey] = []
-            print(lastkey)
         elif line[:-1] == 'keykey':
             readKey = True
         else:
             data[lastkey].append(int(line[:-1]))
-
     del store
     return data
         
@@ -139,5 +138,128 @@ def WriteDataToFile(storeData):
     writer.close()
 
 # 将原始数据复制，然后归一化处理
-
+# 去除outlier，先排序，然后对上下数量分别为1%的点进行最大/最小值抑制
 # 第三部分，聚类
+
+# 对于给定的列表，抑制前ratio%和后ratio%的数据
+# data会直接修改
+#def suppressMinAndMax(data,ratio):
+#    anotherdata = sorted(data)#顺序排序
+#    num = len(anotherdata)
+#    minVal = anotherdata[int(num*ratio)]
+#    maxVal = anotherdata[-1*int(num*ratio)]
+#    for i in range(len(data)):
+#        if data[i]<minVal:
+#           data[i] = minVal
+#        elif data[i]>maxVal:
+#           data[i] = maxVal
+#
+    
+#转成tslearn格式
+import time
+data = ReadDataFromFile()
+
+from tslearn.utils import to_time_series_dataset
+formatted_dataset = to_time_series_dataset(list(data.values()))
+
+#归一化
+from tslearn.preprocessing import TimeSeriesScalerMinMax
+scaler =  TimeSeriesScalerMinMax(value_range=(0., 1.))
+storeMinMax = []
+for i in range(len(formatted_dataset)):
+    ele = formatted_dataset[i]
+    ele = ele.reshape(ele.shape[0])
+    storeMinMax.append((min(ele),max(ele)))
+    formatted_dataset[i] = scaler.fit_transform(ele).reshape(formatted_dataset[i].shape[0],formatted_dataset[i].shape[1])
+
+#进行聚类
+
+## 目标：将已有的时序数据分成若干类，每个类中有一个中心变量，通过对中心变量的预测可以实现对其他时序数据的预测，从而降低时序数据预测的开销
+## 暂时不考虑时序上的位移，只考虑数值上每个类与该类之间存在固定的线性变化，这样通过计算线性关系的算法可以直接算出来
+# T1：直接对原始数据进行基于切分的聚类时不可行的，速度太慢。
+
+
+# 想法一：进行普通归一化。然后以欧式距离/相关性进行聚类，聚类方法为普通K-means递进/DBSCAN/层次聚类
+
+# 想法二：进行最大最小值抑制的归一化，去除噪点，然后同上。
+# 异常点数量定为前后1%，先排序，然后设置阈值，再进行排除
+# 抑制最小最大值的归一化
+
+#进行最小最大值抑制
+import numpy as np
+ratio = 0.01#抑制前1%与后1%的值
+stdData = formatted_dataset.copy()
+for i in range(len(stdData)):
+    ele = stdData[i].copy()
+    ele = ele.reshape(ele.shape[0])
+    ele.sort()
+    minNum,maxNum = ele[int(ratio*len(ele))],ele[-1*int(ratio*len(ele))]
+    del ele
+    def suppressMinandMax(ele):
+        if ele<minNum:
+            return minNum
+        elif ele>maxNum:
+            return maxNum
+        else:
+            return ele
+    cache = stdData[i]
+    stdData[i] = np.fromiter(map(suppressMinandMax,cache.reshape(cache.shape[0])),dtype = np.float64).reshape(cache.shape)
+
+#归一化
+from tslearn.preprocessing import TimeSeriesScalerMinMax
+scaler =  TimeSeriesScalerMinMax(value_range=(0., 1.))
+storeMinMax = []
+for i in range(len(stdData)):
+    ele = stdData[i]
+    ele = ele.reshape(ele.shape[0])
+    storeMinMax.append((min(ele),max(ele)))
+    stdData[i] = scaler.fit_transform(ele).reshape(stdData[i].shape)
+
+# 想法三：进行归一化，然后做FFT或小波变化，然后再进行聚类（可以用这个来验证聚类算法）
+
+# 对一个numpy数组进行DCT
+from numpy import fft
+
+
+# 想法四：利用PAA等技术
+# 然后使用sklearn等库
+from tslearn.piecewise import PiecewiseAggregateApproximation
+from tslearn.piecewise import SymbolicAggregateApproximation, OneD_SymbolicAggregateApproximation
+import time
+# 1dSAX
+n_paa_segments = 40
+n_sax_symbols_avg = 10
+n_sax_symbols_slope = 10
+one_d_sax = OneD_SymbolicAggregateApproximation(
+    n_segments=n_paa_segments,
+    alphabet_size_avg=n_sax_symbols_avg,
+    alphabet_size_slope=n_sax_symbols_slope)
+transformed_data = one_d_sax.fit_transform(stdData)
+# transformed_data是一个 N\*40*2的矩阵，可以reshape成N*80的矩阵
+transformed_data = transformed_data.reshape(transformed_data.shape[0],transformed_data.shape[1]*transformed_data.shape[2])
+
+from sklearn.cluster import MiniBatchKMeans,KMeans,DBSCAN,SpectralClustering,Birch
+from sklearn.metrics import calinski_harabasz_score
+
+#Kmeans 结果
+
+n_cluster = 100
+s = time.time()
+km = KMeans(n_clusters = n_cluster,random_state = 0)
+y_pre = km.fit_predict(transformed_data)
+e = time.time()
+print(e-s,"s")
+print(davies_bouldin_score(transformed_data,y_pre))
+
+# MiniBatch k means
+n_cluster = 100
+s = time.time()
+km = MiniBatchKMeans(n_clusters = n_cluster,init_size = 3*n_cluster,random_state = 0,batch_size = 10)
+y_pre = km.fit_predict(transformed_data)
+e = time.time()
+print(e-s,"s")
+print(davies_bouldin_score(transformed_data,y_pre))
+
+#
+
+# DBSCAN
